2bbad353960cd6367c5443027187f8bda4dde7d9
Alexander <nemechekalexander@gmail.com>
2019-02-07 20:17:32 +0200

    update

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div className="about">
             <h4>WELCOME TO THE LIVE CURRENCY EXCHANGE-RATE WEBSITE</h4>
             <h2 className = 'redh2'>NEVER PAY MORE FOR FOREIGN CURRENCY AGAIN</h2>
-            <h4>SEE THE LATEST EXCHANGE RATES FROM EURO TO USD AND ALL THE OTHER WORLD CURRENCIES </h4>
+            <h4>SEE THE LATEST EXCHANGE RATES OF ALL THE  WORLD CURRENCIES FROM THE EURO BASED FIXER API</h4>
             
         </div>
     )
--- src/ExchangeList.js
+++ src/ExchangeList.js
@@ -19,25 +19,30 @@ const ExchangeList = props => {
                      placeholder = 'amount' 
                      onChange = {props.handleChange} 
                      value = {props.inputValue1}
-                     name = 'inputValue1' /> 
+                     name = 'inputValue1'
+                     required /> 
                 <input type = 'text'
                      className = 'cur1' 
                      placeholder = '$$$' 
                      onChange = {props.handleChange} 
                      value = {props.inputValue}
-                     name = 'inputValue' /> <h1 className = "to">to</h1>
+                     name = 'inputValue' 
+                     maxlength="3"
+                     required/> <h1 className = "to">to</h1>
                 <input type = 'text'
                      className = 'cur2'
                      placeholder = '$$$' 
                      onChange = {props.handleChange}
                      value = {props.inputValue2}
-                     name = 'inputValue2' />
+                     name = 'inputValue2'
+                     maxlength="3"
+                     required />
                 <button>Convert</button>
                     
             </form>
             <div className = 'msg'>
                 <h3 className = 'search'>  { props.conversion && "exchange rate:"  }</h3>
-                 <p>{props.conversion && `${props.inputValue1}${props.input1.toUpperCase()} = ${props.conversion} ${props.input2.toUpperCase()}`}</p>
+                 <p>{props.conversion && `${props.inputValue1} ${props.input1.toUpperCase()} = ${props.conversion} ${props.input2.toUpperCase()}`}</p>
             </div> 
                                      
              
